cc524688a74a054416d133ae86e3d3df3c8c5680
nikiul <nikulans@gmail.com>
2020-12-02 16:12:58 +0300

    added video

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@
 var bg = document.getElementById('menu-icon-line');
 document.getElementById('header__menu').addEventListener("mouseover", function(){bg.style.background = "#006CB5";this.addEventListener("mouseout", function(){bg.style.background = "";});
 });
+
+// Видео
+$(document).on('click','.js-videoPoster',function(e) {
+    //отменяем стандартное действие button
+    e.preventDefault();
+    var poster = $(this);
+    // ищем родителя ближайшего по классу
+    var wrapper = poster.closest('.js-videoWrapper');
+    videoPlay(wrapper);
+  });
+  
+  //вопроизводим видео, при этом скрывая постер
+  function videoPlay(wrapper) {
+    var iframe = wrapper.find('.js-videoIframe');
+    // Берем ссылку видео из data
+    var src = iframe.data('src');
+    // скрываем постер
+    wrapper.addClass('videoWrapperActive');
+    // подставляем в src параметр из data
+    iframe.attr('src',src);
+  }
